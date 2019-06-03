986f97d10c987822345d658f259c4f08627c5c44
Jonathan Harris <jonharris85@gmail.com>
2019-06-03 11:21:37 -0400

    Update engine.js

--- Retro Arcade Game/js/engine.js
+++ Retro Arcade Game/js/engine.js
@@ -1,24 +1,7 @@
-/* Engine.js
- * This file provides the game loop functionality (update entities and render),
- * draws the initial game board on the screen, and then calls the update and
- * render methods on your player and enemy objects (defined in your app.js).
- *
- * A game engine works by drawing the entire game screen over and over, kind of
- * like a flipbook you may have created as a kid. When your player moves across
- * the screen, it may look like just that image/character is moving or being
- * drawn but that is not the case. What's really happening is the entire "scene"
- * is being drawn over and over, presenting the illusion of animation.
- *
- * This engine makes the canvas' context (ctx) object globally available to make
- * writing app.js a little simpler to work with.
- */
-
-var Engine = (function(global) {
-    /* Predefine the variables we'll be using within this scope,
-     * create the canvas element, grab the 2D context for that canvas
-     * set the canvas element's height/width and add it to the DOM.
-     */
-    var doc = global.document,
+let Engine = (function(global) {
+    // Predefine the variables we'll be using within this scope and create a canvas
+     
+    let doc = global.document,
         win = global.window,
         canvas = doc.createElement('canvas'),
         ctx = canvas.getContext('2d'),
@@ -28,9 +11,8 @@ var Engine = (function(global) {
     canvas.height = 606;
     doc.body.appendChild(canvas);
 
-    /* This function serves as the kickoff point for the game loop itself
-     * and handles properly calling the update and render methods.
-     */
+    // This function is a kick off point for the game to loop itself and handles update and render methods.
+     
     function main() {
         /* Get our time delta information which is required if your game
          * requires smooth animation. Because everyone's computer processes
@@ -63,7 +45,6 @@ var Engine = (function(global) {
      * game loop.
      */
     function init() {
-        init.allEnemies;
         reset();
         lastTime = Date.now();
         main();
@@ -117,10 +98,11 @@ var Engine = (function(global) {
             ],
             numRows = 6,
             numCols = 5,
-            row, col;
+            row,
+            col;
 
         // Before drawing, clear existing canvas
-        ctx.clearRect(0,0,canvas.width,canvas.height);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
 
         /* Loop through the number of rows and columns we've defined above
          * and, using the rowImages array, draw the correct image for that
@@ -184,6 +166,4 @@ var Engine = (function(global) {
      */
     global.ctx = ctx;
 })(this);
-
-
 
